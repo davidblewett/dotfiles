d1be0ae8cf2e967d1e8437e6f8953306b47f7354
David Blewett <david.blewett@crowdstrike.com>
2017-11-02 10:47:39 -0400

    Update create_links.

--- create_links.sh
+++ create_links.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# Exit after first error
+set -e
+
 ########################################
 # This is the list of conf files that we
 # want to automagically link after
@@ -10,26 +13,29 @@
 # 'cleanup' means to remove all the files created by this script
 remove="$1"
 
-dotfiles_loc=$(cd `dirname $0` && pwd)
+dotfiles_loc=$(cd "$(dirname "$0")" && pwd)
 excluded=(
     .svn
     .git
     .DS_Store
     .
     ..
+    .AppleDouble
 )
 # make sure the dotfiles are only rwx by the owner
-chmod 700 $dotfiles_loc
+chmod 700 "$dotfiles_loc"
+# make sure my home dir is secured
+chmod 700 "$HOME"
 
 notExcluded() {
     # check that the value exists
     if [ -z "$1" ]; then
         return
     fi
-    
-    for i in ${excluded[@]}
+
+    for i in "${excluded[@]}"
     do
-        if [ $i == $1 ]; then
+        if [ "$i" == "$1" ]; then
             # the item exists
             return 1
         fi
@@ -52,7 +58,7 @@ linkDotfile() {
         # symlink the conf file
         if [ ! -e "$to_create" ]; then
             echo "linking $dotfile"
-            ln -s $actual_dotfile $to_create
+            ln -s "$actual_dotfile" "$to_create"
         fi
         # warn the user that an existing file is in the way
         if [ ! -h "$to_create" -a -e "$to_create" ]; then
@@ -65,11 +71,11 @@ linkDotfile() {
 # -----------------------------------------------------------------
 for actual_dotfile in $HOME/.dotfiles/.*
     do
-        dotfile=$(echo $actual_dotfile | awk -F"$dotfiles_loc/" '{print $2}')
+        dotfile=$(echo "$actual_dotfile" | awk -F"$dotfiles_loc/" '{print $2}')
         # ignore certain directories
-        if notExcluded $dotfile; then
+        if notExcluded "$dotfile"; then
             to_create="$HOME/$dotfile"
-            linkDotfile $dotfile $to_create $actual_dotfile
+            linkDotfile "$dotfile" "$to_create" "$actual_dotfile"
         fi
     done
 
@@ -78,34 +84,76 @@ for actual_dotfile in $HOME/.dotfiles/.*
 #       aren't under version control.
 ###################################################################
 
+# make the .subversion dir if it doesn't exist already
+[ ! -d "$HOME/.subversion" ] && mkdir "$HOME/.subversion"
+
 # take care of the .subversion/config file
 # -----------------------------------------------------------------
-actual_dotfile="$dotfiles_loc/svn_config"
+actual_dotfile="$dotfiles_loc/config"
 dotfile="config"
 to_create="$HOME/.subversion/$dotfile"
 # actually create/remove the link
-linkDotfile $dotfile $to_create $actual_dotfile
+linkDotfile "$dotfile" "$to_create" "$actual_dotfile"
 
-# take care of the .ssh/config file
+# take care of the .subversion/servers file
 # -----------------------------------------------------------------
-actual_dotfile="$dotfiles_loc/ssh_config"
-dotfile="config"
-to_create="$HOME/.ssh/$dotfile"
+actual_dotfile="$dotfiles_loc/servers"
+dotfile="servers"
+to_create="$HOME/.subversion/$dotfile"
 # actually create/remove the link
-linkDotfile $dotfile $to_create $actual_dotfile
+linkDotfile "$dotfile" "$to_create" "$actual_dotfile"
 
-# take care of the .ssh/authorized_keys file
+# take care of the ~/.ssh/authorized_keys
 # -----------------------------------------------------------------
+# make the .ssh dir if it doesn't exist already
+[ ! -d "$HOME/.ssh" ] && mkdir "$HOME/.ssh" && chmod 700 "$HOME/.ssh"
 actual_dotfile="$dotfiles_loc/authorized_keys"
-dotfile="authorized_keys"
-to_create="$HOME/.ssh/$dotfile"
-# actually create/remove the link
-linkDotfile $dotfile $to_create $actual_dotfile
+to_create="$HOME/.ssh/authorized_keys"
+# remove the old symlink if it is there
+[ -h "$to_create" ] && rm "$to_create" && echo "removed symlink for $to_create"
+# Create the file if it doesn't exist
+[ ! -e "$to_create" ] && touch "$to_create"
+# For each key, see if it exists, if not, add it
+while read skey; do
+if ! grep -q "$skey" "$to_create"; then
+      echo "$skey" >> "$to_create"
+      key_name=$(echo "$skey" | awk '{print $3}')
+      echo "Added $key_name"
+  fi
+done < "$actual_dotfile"
+
+# Ensure that StrictModes doesn't cause us issues with SSH key auth
+if [ -e "$HOME/.ssh/authorized_keys" ]; then
+    chmod 600 "$HOME/.ssh/authorized_keys"
+fi
+
+# Take care of awesome configs
+# -----------------------------------------------------------------
+if [ -d "$HOME/.config" ]; then
+    actual_dotfile="$dotfiles_loc/awesome"
+    dotfile="awesome"
+    to_create="$HOME/.config/$dotfile"
+    # actually create/remove the link
+    linkDotfile "$dotfile" "$to_create" "$actual_dotfile"
+fi
+
+# Take care of neovim config
+# -----------------------------------------------------------------
+if [ -d "$HOME/.config" ]; then
+    actual_dotfile="$dotfiles_loc/.vim"
+    dotfile="nvim"
+    to_create="$HOME/.config/$dotfile"
+    # actually create/remove the link
+    linkDotfile "$dotfile" "$to_create" "$actual_dotfile"
+fi
+
+# Install the vim plugins
+which vim && vim -c PlugInstall -c qa
 
 # NOTE: None of these files are under version control...
 # -----------------------------------------------------------------
 if [ ! "$remove" = "remove" ]; then
-    
+
     # warn the user what is about to happen
     if [ "$remove" = "cleanup" ]; then
         echo '
@@ -115,7 +163,7 @@ Deleting them will remove them immediately
 ******************************************
 '
     fi
-    
+
     # touch extra files needed by the confs
     # -------------------------------------------------------------
     touch_me=(
@@ -124,6 +172,7 @@ Deleting them will remove them immediately
         .zshextras
         .zshenv_extras
         .bash_extras
+        .tmux.local
     )
     for to_touch in "${touch_me[@]}"
         do
@@ -145,10 +194,11 @@ Deleting them will remove them immediately
                 chmod 600 "$TOUCH_FILE"
             fi
         done
-    
+
     DIRS_TO_MAKE=(
         "$HOME"/bin
         "$HOME"/.backup
+        "$HOME"/.backup/vim/undo
         "$HOME"/.backup/vim/swap
         "$HOME"/.virtualenvs
         "$HOME"/.buildout
@@ -157,7 +207,7 @@ Deleting them will remove them immediately
         "$HOME"/.buildout/zope
         "$HOME"/.buildout/extends
     )
-    
+
     # create the $DIRS_TO_MAKE
     # -------------------------------------------------------------
     processDotDir() {
@@ -181,18 +231,18 @@ Deleting them will remove them immediately
             echo "something in the way of $dot_directory being created"
         fi
     }
-    
+
     for dir in "${DIRS_TO_MAKE[@]}"
         do
-            processDotDir $dir
+            processDotDir "$dir"
         done
-    
+
     # create a buildout directory structure
     # -------------------------------------------------------------
     BUILDOUT_DIR="$HOME"/.buildout
     if [[ -d "$BUILDOUT_DIR" ]] && [ ! -e "$BUILDOUT_DIR/default.cfg" ]; then
         # create the default.cfg file
-        cat > $BUILDOUT_DIR/default.cfg <<EOF
+        cat > "$BUILDOUT_DIR/default.cfg" <<EOF
 #[buildout]
 #eggs-directory = $HOME/.buildout/eggs
 #download-cache = $HOME/.buildout/downloads
@@ -212,7 +262,7 @@ EOF
     FILES_TO_MAKE=(
         "$HOME"/.pypirc
     )
-    
+
     # create some uncontrolled files
     # -------------------------------------------------------------
     processDotFiles() {
@@ -235,7 +285,7 @@ EOF
                 (
                 cat <<EOF
 #[distutils]
-#index-servers = 
+#index-servers =
 #    pypi
 #    skillet
 #    plone.org
@@ -256,13 +306,15 @@ EOF
 EOF
 ) > "$dot_file"
             fi
-    fi
+
+        echo "Created $dot_file example"
+        chmod 600 "$dot_file"
+        fi
     }
-    
+
     for dot_file in "${FILES_TO_MAKE[@]}"
         do
-            processDotFiles $dot_file
+            processDotFiles "$dot_file"
         done
-    
-fi
 
+fi
